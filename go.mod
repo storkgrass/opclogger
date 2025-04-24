de41c7e011db27db4e5db0fe2e3ed13d808e54f0
hariya <hariya.yosuke@nihondengi.co.jp>
2025-04-24 12:08:20 +0900

    Add service implementation using kardianos/service

--- go.mod
+++ go.mod
@@ -8,4 +8,9 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/kardianos/service v1.2.2
+)
+
+require golang.org/x/sys v0.28.0 // indirect
--- main.go
+++ main.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/gopcua/opcua"
 	"github.com/gopcua/opcua/ua"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
+	"github.com/kardianos/service"
 	_ "github.com/lib/pq"
 
 	"github.com/storkgrass/opclogger/config"
 )
+
+var logger service.Logger
+
+type program struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	wg     sync.WaitGroup
+}
+
 type EnvVars struct {
 	DATABASE_URL          string
 	OPCUA_ENDPOINT        string
@@ -32,45 +43,123 @@ type EnvVars struct {
 	DATABASE_TIMEOUT      int
 }
 
+func (p *program) Start(s service.Service) error {
+	p.ctx, p.cancel = context.WithCancel(context.Background())
+
+	go func() {
+		if err := p.run(); err != nil {
+			logger.Errorf("Run error: %v", err)
+			p.cancel()
+		}
+	}()
+
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	// Any work in Stop should be quick, usually a few seconds at most.
+	logger.Info("stopping...")
+	p.cancel()
+
+	done := make(chan struct{})
+	go func() {
+		p.wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		logger.Info("Graceful shutdown complete.")
+	case <-time.After(2 * time.Second):
+		logger.Warning("Forced shutdown due to timeout.")
+	}
+
+	return nil
+}
+
 func main() {
-	err := godotenv.Load()
+	svcFlag := flag.String("service", "", "Control the system service.")
+	flag.Parse()
+
+	options := make(service.KeyValue)
+	options["Restart"] = "on-failure"
+	options["RestartSec"] = "3"
+	options["SuccessExitStatus"] = "0 SIGTERM"
+	options["StandardOutput"] = "journal"
+	svcConfig := &service.Config{
+		Name:        "opclogger-service",
+		DisplayName: "OPC UA Data Logging Service",
+		Description: "A service that collects data from an OPC UA server and logs it to a database.",
+		Dependencies: []string{
+			"Requires=network.target",
+			"After=network-online.target syslog.target"},
+		Option: options,
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
 	if err != nil {
-		log.Fatalf("failed to loading .env file: %v", err)
+		log.Fatal(err)
+	}
+	errs := make(chan error, 5)
+	logger, err = s.Logger(errs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			select {
+			case err := <-errs:
+				if err != nil {
+					logger.Errorf("service error: %v", err)
+				}
+			case <-prg.ctx.Done():
+				return
+			}
+		}
+	}()
+
+	if len(*svcFlag) != 0 {
+		err := service.Control(s, *svcFlag)
+		if err != nil {
+			log.Printf("Failed to control service with action %s: %v", *svcFlag, err)
+			log.Printf("Valid actions: %q", service.ControlAction)
+			log.Fatal(err)
+		}
+		return
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
+
+func (p *program) run() error {
+	envVars, err := loadEnvironment()
+	if err != nil {
+		return err
 	}
 
 	cfg, err := config.LoadConfig("config.json")
 	if err != nil {
-		log.Fatalf("failed to load the config.json: %v", err)
+		return fmt.Errorf("failed to load the config.json: %v", err)
+	}
+	if err := cfg.Validate(); err != nil {
+		return fmt.Errorf("failed to validate the config.json: %v", err)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	var wg sync.WaitGroup
-
-	// Listen for OS interrupt or termination signals and trigger graceful shutdown
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		// Block until a signal is received
-		sig := <-sigs
-		log.Printf("received OS signal: %s. Initiating graceful shutdown...", sig)
-		cancel()
-	}()
-
 	// Establish a connection to the PostgreSQL database
-	dbURL := os.Getenv("DATABASE_URL")
-	db, err := sqlx.Connect("postgres", dbURL)
+	db, err := initDatabase(p.ctx, envVars.DATABASE_URL)
 	if err != nil {
-		log.Fatalf("failed to connect to the database: %v", err)
+		return fmt.Errorf("failed to initialize database: %v", err)
 	}
 	defer db.Close()
 
 	// Establish a connection to the OPC UA server
-	opcURL := os.Getenv("OPCUA_ENDPOINT")
-	opcSecurityPolicy := os.Getenv("OPCUA_SECURITY_POLICY")
-	opcSecurityMode := os.Getenv("OPCUA_SECURITY_MODE")
-	client, err := newOPCUAClient(opcURL, opcSecurityPolicy, opcSecurityMode, ctx)
+	client, err := initOPCUAClient(p.ctx, envVars.OPCUA_ENDPOINT, envVars.OPCUA_SECURITY_POLICY, envVars.OPCUA_SECURITY_MODE)
 	if err != nil {
-		log.Fatalf("failed to initialize OPC UA client: %v", err)
+		return fmt.Errorf("failed to initialize OPC UA client: %v, endpoint: %s, security policy: %s, security mode: %s", err, envVars.OPCUA_ENDPOINT, envVars.OPCUA_SECURITY_POLICY, envVars.OPCUA_SECURITY_MODE)
 	}
 	defer client.Close(ctx)
 
@@ -81,25 +170,34 @@ func main() {
 	}
 
 	// Main Loop
-	for interval, tables := range intervalMap {
-		wg.Add(1)
+	for interval, groups := range intervalMap {
+		p.wg.Add(1)
 		go func(interval int, groups []config.TagGroup, ctx context.Context) {
-			defer wg.Done()
+			defer p.wg.Done()
 			ticker := time.NewTicker(time.Duration(interval) * time.Second)
 			defer ticker.Stop()
 
 			for {
 				select {
 				case <-ticker.C:
-					for _, gp := range groups {
-						err := readValues(gp.Tags, client, ctx)
-						if err != nil {
-							//TODO
+					for i := range groups {
+						if err := readValues(ctx, groups[i].Tags, client); err != nil {
+							if state := client.State(); state == opcua.Disconnected {
+								logger.Warningf("OPC UA client is disconnected, retrying connection...")
+								if err := retryOpcuaConnect(ctx, client, envVars.OPCUA_MAXRETRIES, time.Duration(envVars.OPCUA_TIMEOUT)*time.Second); err != nil {
+									logger.Errorf("failed to reconnect to the OPC UA server: %v, endpoint: %s", err, envVars.OPCUA_ENDPOINT)
+								}
+							} else {
+								logger.Errorf("failed to read values: %v, tag group: %s", err, groups[i].TableName)
+							}
 							continue
 						}
-						err = writeDatabase(gp, db, ctx)
-						if err != nil {
-							//TOOD
+
+						if err = writeDatabase(ctx, envVars.TIME_COLUMN_NAME, groups[i], db); err != nil {
+							if err := retryDatabaseConnect(ctx, db, envVars.DATABASE_MAXRETRIES, time.Duration(envVars.DATABASE_TIMEOUT)*time.Second); err != nil {
+								logger.Errorf("failed to reconnect to the database: %v", err)
+							}
+							logger.Errorf("failed to write to database: %v", err)
 							continue
 						}
 					}
@@ -107,8 +205,11 @@ func main() {
 					return
 				}
 			}
-		}(interval, tables, ctx)
+		}(interval, groups, p.ctx)
 	}
+
+	return nil
+}
 
 func getEnvOrDefault(key, defaultValue string) string {
 	value, exists := os.LookupEnv(key)
@@ -170,9 +271,19 @@ func loadEnvironment() (*EnvVars, error) {
 
 	return envVars, nil
 }
+
+func initDatabase(ctx context.Context, dbURL string) (*sqlx.DB, error) {
+	db, err := sqlx.ConnectContext(ctx, "postgres", dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to the database: %v", err)
+	}
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping the database: %v", err)
+	}
+	return db, nil
 }
 
-func newOPCUAClient(endpointURL string, securityPolicy string, securityMode string, ctx context.Context) (*opcua.Client, error) {
+func initOPCUAClient(ctx context.Context, endpointURL string, securityPolicy string, securityMode string) (*opcua.Client, error) {
 	opcOpts := []opcua.Option{
 		opcua.SecurityPolicy(securityPolicy),
 		opcua.SecurityModeString(securityMode),
@@ -188,7 +299,50 @@ func newOPCUAClient(endpointURL string, securityPolicy string, securityMode stri
 	return client, nil
 }
 
-func readValues(tags []config.Tag, client *opcua.Client, ctx context.Context) error {
+func retryOpcuaConnect(ctx context.Context, client *opcua.Client, maxRetries int, timeout time.Duration) error {
+	return retryWithExponentialBackoff(ctx, "connect to OPC UA server", maxRetries, func(ctx context.Context) error {
+		return client.Connect(ctx)
+	}, timeout)
+}
+
+func retryDatabaseConnect(ctx context.Context, db *sqlx.DB, maxRetries int, timeout time.Duration) error {
+	return retryWithExponentialBackoff(ctx, "connect to database", maxRetries, func(ctx context.Context) error {
+		return db.PingContext(ctx)
+	}, timeout)
+}
+
+func retryWithExponentialBackoff(ctx context.Context, operation string, maxRetries int, fn func(context.Context) error, timeout time.Duration) error {
+	if maxRetries < 1 {
+		return fmt.Errorf("maxRetries must be greater than 0")
+	}
+	if timeout < 1 {
+		return fmt.Errorf("timeout must be greater than 0")
+	}
+
+	var err error
+	for retry := range maxRetries {
+		logger.Infof("[%d/%d] Retrying operation: %s", retry+1, maxRetries, operation)
+		timeoutCtx, cancel := context.WithTimeout(ctx, timeout)
+		err = fn(timeoutCtx)
+		cancel()
+		if err == nil {
+			return nil
+		}
+
+		// Simple exponential backoff strategy: 2^retry seconds, up to 60 seconds
+		backoffDuration := time.Duration(math.Min(math.Pow(2, float64(retry)), 60)) * time.Second
+
+		select {
+		case <-time.After(backoffDuration):
+			continue
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return fmt.Errorf("failed to %s after %d retries: %v", operation, maxRetries, err)
+}
+
+func readValues(ctx context.Context, tags []config.Tag, client *opcua.Client) error {
 	nodeIDs := make([]*ua.ReadValueID, 0, len(tags))
 	for i := range tags {
 		nodeIDs = append(nodeIDs, &ua.ReadValueID{
@@ -211,8 +365,12 @@ func readValues(tags []config.Tag, client *opcua.Client, ctx context.Context) er
 		return err
 	}
 
-	if resp.Results == nil || len(resp.Results) != len(tags) {
-		return fmt.Errorf("invalid response: %v", resp.Results)
+	if resp.Results == nil {
+		return fmt.Errorf("received nil results from OPC UA read operation for tags: %v", tags)
+	}
+
+	if len(resp.Results) != len(tags) {
+		return fmt.Errorf("expected %d results but got %d for tags: %v", len(tags), len(resp.Results), tags)
 	}
 
 	for i := range tags {
@@ -226,7 +384,7 @@ func readValues(tags []config.Tag, client *opcua.Client, ctx context.Context) er
 	return nil
 }
 
-func writeDatabase(group config.TagGroup, db *sqlx.DB, ctx context.Context) error {
+func writeDatabase(ctx context.Context, timeColumn string, group config.TagGroup, db *sqlx.DB) error {
 	timeoutCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
 
@@ -234,7 +392,7 @@ func writeDatabase(group config.TagGroup, db *sqlx.DB, ctx context.Context) erro
 		return nil
 	}
 
-	columns := []string{"time"}
+	columns := []string{timeColumn}
 	values := []any{time.Now()}
 	for _, tag := range group.Tags {
 		if tag.Value != nil {
